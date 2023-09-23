2b2f545ef10776787b5a0d49148ddbb6154c7428
TengMCing <llreczx@gmail.com>
2023-09-23 22:59:42 +1000

    doc misc.c

--- pl_misc.c
+++ pl_misc.c
@@ -4,10 +4,16 @@
 
 #include "pl_misc.h"
 
+#ifdef PL_TEST
+    #include "pl_unittest.h"
+#endif//PL_TEST
+
 /*-----------------------------------------------------------------------------
  |  Compare address (mainly used by qsort)
  ----------------------------------------------------------------------------*/
 
+/// Compare two chars.
+/// @return 1 if a > b, -1 if a \< b, 0 otherwise.
 static int compare_char(const void *const a, const void *const b)
 {
     const char arg1 = ((const char *) a)[0];
@@ -19,6 +25,8 @@ static int compare_char(const void *const a, const void *const b)
     return 0;
 }
 
+/// Compare two ints.
+/// @return 1 if a > b, -1 if a \< b, 0 otherwise.
 static int compare_int(const void *const a, const void *const b)
 {
     const int arg1 = ((const int *) a)[0];
@@ -30,6 +38,8 @@ static int compare_int(const void *const a, const void *const b)
     return 0;
 }
 
+/// Compare two doubles.
+/// @return 1 if a > b, -1 if a \< b, 0 otherwise.
 static int compare_double(const void *const a, const void *const b)
 {
     const double arg1 = ((const double *) a)[0];
@@ -41,6 +51,8 @@ static int compare_double(const void *const a, const void *const b)
     return 0;
 }
 
+/// Compare two addresses.
+/// @return 1 if a > b, -1 if a \< b, 0 otherwise.
 static int compare_address(const void *const a, const void *const b)
 {
     if (a > b)
@@ -50,15 +62,43 @@ static int compare_address(const void *const a, const void *const b)
     return 0;
 }
 
+static pl_unittest_summary test_compare(void)
+{
+    pl_unittest_summary summary = pl_unittest_new_summary();
+
+    char char_x[1] = {'c'};
+    char char_y[1] = {'b'};
+    pl_unittest_expect_true(summary, compare_char(char_x, char_y) == 1);
+    pl_unittest_expect_true(summary, compare_char(char_y, char_y) == 0);
+    pl_unittest_expect_true(summary, compare_char(char_y, char_x) == -1);
+
+    return summary;
+}
+
 /*-----------------------------------------------------------------------------
  |  Get misc namespace
  ----------------------------------------------------------------------------*/
 
+static void test(void)
+{
+    printf("In file: %s\n", __FILE__);
+    pl_unittest_print_summary(test_compare());
+}
+
 pl_misc_ns pl_misc_get_ns(void)
 {
+#ifdef PL_TEST
+    static const pl_misc_ns misc_ns = {.compare_char    = compare_char,
+                                       .compare_int     = compare_int,
+                                       .compare_double  = compare_double,
+                                       .compare_address = compare_address,
+                                       .test            = test};
+#else
     static const pl_misc_ns misc_ns = {.compare_char    = compare_char,
                                        .compare_int     = compare_int,
                                        .compare_double  = compare_double,
                                        .compare_address = compare_address};
+#endif//PL_TEST
+
     return misc_ns;
 }
