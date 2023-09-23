9babb76410693c7d0fd588950cb7b021aeeb59e4
TengMCing <llreczx@gmail.com>
2023-09-23 23:02:12 +1000

    modify pl main namespace

--- pl.c
+++ pl.c
@@ -10,8 +10,11 @@
 
 pl_ns pl_get_ns(void)
 {
-    const pl_ns ns = {.misc  = pl_misc_get_ns(),
-                      .error = pl_error_get_ns(),
-                      .bt    = pl_bt_get_ns()};
+    const pl_ns ns = {.misc   = pl_misc_get_ns(),
+                      .error  = pl_error_get_ns(),
+                      .class = pl_class_get_ns(),
+                      .gc     = pl_gc_get_ns(),
+                      .object = pl_object_get_ns(),
+                      .var    = pl_var_get_ns()};
     return ns;
 }
