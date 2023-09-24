d580204981b64fe64ad0250081baa2bc13a9007b
TengMCing <llreczx@gmail.com>
2023-09-25 09:57:08 +1000

    update main.c

--- main.c
+++ main.c
@@ -1,22 +1,105 @@
 #include "pl.h"
 #include "stdio.h"
 
-static pl_optional_double test2(void)
+// static pl_object range(int end)
+// {
+//     pl_ns pl = pl_get_ns();
+//     declare_variables(x)
+//     {
+//         pl_misc_for_i(end)
+//         {
+//         }
+//     }
+// }
+
+
+void test(void)
 {
-    return (pl_optional_double){.value = 2.0};
+    // Get the namespace.
+    pl_ns pl = pl_get_ns();
+
+    pl.class.test();
+    pl.error.test();
+    pl.misc.test();
 }
 
-static pl_optional_int test(void)
-{
-    pl_optional_double x = pl_bt_call(pl_optional_double, test2);
-    pl_error_pass_to_caller(x.error, pl_optional_int);
-    return (pl_optional_int){.value = (int) x.value};
-}
 
 int main()
 {
+
+    // Get the namespace.
     pl_ns pl = pl_get_ns();
-    pl_optional_int x = pl_bt_call(pl_optional_int, test);
-    pl.error.optionally_print_and_exit(x.error);
-    return pl.misc.compare_address(NULL, NULL);
+
+    test();
+
+    return 0;
+
+    // In R: v <- c(1, 2, 3)
+    pl.var.set("v", c(1, 2, 3), 0);
+
+    // In R: v <- c(4, 5, 6)
+    pl.var.set("v", c(4, 5, 6), 0);
+
+    // In R: l <- list(c('a', 'b', 'c'), c(1, 2, 3, NA))
+    pl.var.set("l", list(c(*"a", 'b', 'c'), c(1, 2, 3, PL_INT_NA)), 0);
+
+    // In R: print(c(4) %in% v)
+    // stdout: [0]
+    pl.object.print(pl.object.in(local_c(4), pl.var.get("v", 0)));
+
+    // In R: print(v[c(1, 3, 3, 2)])
+    // stdout: [1, 3, 3, 2]
+    pl.object.print(pl.object.subset(pl.var.get("v", 0), local_c(0, 2, 2, 1)));
+
+    // In R: print(v[[c(1)]])
+    // stdout: [1]
+    pl.object.print(pl.object.extract(pl.var.get("v", 0), local_c(0)));
+
+    // In R: print(l[[c(1)]])
+    // stdout: ['a', 'b', 'c']
+    pl.object.print(pl.object.extract(pl.var.get("l", 0), local_c(0)));
+
+    // In R: print(l[[c(2)]])
+    // stdout: [1, 2, 3, NA]
+    pl.object.print(pl.object.extract(pl.var.get("l", 0), local_c(1)));
+
+
+    // In R: l <- append(l, v)
+    pl.object.append(pl.var.get("l", 0), pl.var.get("v", 0));
+
+    // In R: print(l)
+    // stdout: [<CHAR>, <INT>, <INT>]
+    pl.object.print(pl.var.get("l", 0));
+
+    // In R: l2 <- l
+    pl.var.set("l2", pl.object.copy(pl.var.get("l", 0)), 0);
+
+    // Report the memory usage.
+    pl.gc.report();
+
+    pl.var.delete("l2", 0);
+
+    // Run the garbage collector.
+    pl.gc.garbage_collect();
+
+    // Report the memory usage.
+    pl.gc.report();
+
+    pl.var.delete("l", 0);
+
+    // Run the garbage collector.
+    pl.gc.garbage_collect();
+
+    // Report the memory usage.
+    pl.gc.report();
+
+    pl.var.delete("v", 0);
+
+    // Run the garbage collector.
+    pl.gc.garbage_collect();
+
+    // Report the memory usage.
+    pl.gc.report();
+
+    return 0;
 }
