c8c0ed7f97528f3249d45306cf91b9ff1f97e5f1
TengMCing <llreczx@gmail.com>
2023-09-24 23:54:20 +1000

    modify misc test

--- pl_misc.c
+++ pl_misc.c
@@ -25,6 +25,19 @@ static int compare_char(const void *const a, const void *const b)
     return 0;
 }
 
+static pl_unittest_summary test_compare_char(void)
+{
+    pl_unittest_summary summary = pl_unittest_new_summary();
+
+    char x[1] = {'c'};
+    char y[1] = {'b'};
+    pl_unittest_expect_true(summary, compare_char(x, y) == 1);
+    pl_unittest_expect_true(summary, compare_char(y, y) == 0);
+    pl_unittest_expect_true(summary, compare_char(y, x) == -1);
+
+    return summary;
+}
+
 /// Compare two ints.
 /// @return 1 if a > b, -1 if a \< b, 0 otherwise.
 static int compare_int(const void *const a, const void *const b)
@@ -36,6 +49,19 @@ static int compare_int(const void *const a, const void *const b)
     if (arg1 < arg2)
         return -1;
     return 0;
+}
+
+static pl_unittest_summary test_compare_int(void)
+{
+    pl_unittest_summary summary = pl_unittest_new_summary();
+
+    int x[1] = {'c'};
+    int y[1] = {'b'};
+    pl_unittest_expect_true(summary, compare_int(x, y) == 1);
+    pl_unittest_expect_true(summary, compare_int(y, y) == 0);
+    pl_unittest_expect_true(summary, compare_int(y, x) == -1);
+
+    return summary;
 }
 
 /// Compare two doubles.
@@ -51,6 +77,19 @@ static int compare_double(const void *const a, const void *const b)
     return 0;
 }
 
+static pl_unittest_summary test_compare_double(void)
+{
+    pl_unittest_summary summary = pl_unittest_new_summary();
+
+    double x[1] = {2.0};
+    double y[1] = {1.0};
+    pl_unittest_expect_true(summary, compare_double(x, y) == 1);
+    pl_unittest_expect_true(summary, compare_double(y, y) == 0);
+    pl_unittest_expect_true(summary, compare_double(y, x) == -1);
+
+    return summary;
+}
+
 /// Compare two addresses.
 /// @return 1 if a > b, -1 if a \< b, 0 otherwise.
 static int compare_address(const void *const a, const void *const b)
@@ -62,18 +101,21 @@ static int compare_address(const void *const a, const void *const b)
     return 0;
 }
 
-static pl_unittest_summary test_compare(void)
+static pl_unittest_summary test_compare_address(void)
 {
     pl_unittest_summary summary = pl_unittest_new_summary();
 
-    char char_x[1] = {'c'};
-    char char_y[1] = {'b'};
-    pl_unittest_expect_true(summary, compare_char(char_x, char_y) == 1);
-    pl_unittest_expect_true(summary, compare_char(char_y, char_y) == 0);
-    pl_unittest_expect_true(summary, compare_char(char_y, char_x) == -1);
+    int x_int = 0;
+    int y_int = 1;
+    void* x[1] = {&x_int};
+    void* y[1] = {&y_int};
+    pl_unittest_expect_true(summary, compare_address(x, y) == 1);
+    pl_unittest_expect_true(summary, compare_address(y, y) == 0);
+    pl_unittest_expect_true(summary, compare_address(y, x) == -1);
 
     return summary;
 }
+
 
 /*-----------------------------------------------------------------------------
  |  Get misc namespace
@@ -82,7 +124,10 @@ static pl_unittest_summary test_compare(void)
 static void test(void)
 {
     printf("In file: %s\n", __FILE__);
-    pl_unittest_print_summary(test_compare());
+    pl_unittest_print_summary(test_compare_char());
+    pl_unittest_print_summary(test_compare_int());
+    pl_unittest_print_summary(test_compare_double());
+    pl_unittest_print_summary(test_compare_address());
 }
 
 pl_misc_ns pl_misc_get_ns(void)
