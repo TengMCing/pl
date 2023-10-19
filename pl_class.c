b5669bbf42abc67a042bcfa18a86fe9a14456bcd
TengMCing <llreczx@gmail.com>
2023-10-19 16:47:57 +1100

    update and check `pl_class.c`

--- pl_class.c
+++ pl_class.c
@@ -1,6 +1,8 @@
 //
 // Created by Patrick Li on 1/7/2023.
 //
+// Checked by Patrick Li on 10/19/2023
+//
 
 #include "pl_class.h"
 #include "pl_error.h"
@@ -16,18 +18,18 @@
 /// Check if one class is inherited from another class.
 /// @param derived (int). The derived class.
 /// @param base (int). The base class.
-/// @return A boolean.
+/// @return 0 or 1.
+/// @Exceptions PL_ERROR_UNDEFINED_CLASS: Either `derived` or `base` receives
+/// undefined class. No side effects.
 static int inherit(const int derived, const int base)
 {
     pl_error_expect(derived >= 0 && derived < PL_NUM_CLASS,
                     PL_ERROR_UNDEFINED_CLASS,
-                    "Undefined class [%d]!",
-                    derived);
+                    "Undefined class [%d]!", derived);
 
     pl_error_expect(base >= 0 && base < PL_NUM_CLASS,
                     PL_ERROR_UNDEFINED_CLASS,
-                    "Undefined class [%d]!",
-                    base);
+                    "Undefined class [%d]!", base);
 
     // Check all parents of the derived classes.
     int current = derived;
@@ -49,6 +51,7 @@ static pl_unittest_summary test_inherit(void)
     pl_unittest_expect_error_is(summary, PL_ERROR_UNDEFINED_CLASS, inherit(-1, -1));
     pl_unittest_expect_error_is(summary, PL_ERROR_UNDEFINED_CLASS, inherit(PL_NUM_CLASS, -1));
     pl_unittest_expect_error_is(summary, PL_ERROR_UNDEFINED_CLASS, inherit(-1, PL_NUM_CLASS));
+
     pl_unittest_expect_true(summary, inherit(PL_CLASS_CHAR, PL_CLASS_INT) == 0);
     pl_unittest_expect_true(summary, inherit(PL_CLASS_INT, PL_CLASS_INT) == 1);
 
@@ -62,6 +65,8 @@ static pl_unittest_summary test_inherit(void)
 /// Get the underlying type of a class.
 /// @param derived (int). The derived class.
 /// @return The base class.
+/// @Exceptions PL_ERROR_UNDEFINED_CLASS: `derived` receives undefined class.
+/// No side effects.
 static int type(const int derived)
 {
     pl_error_expect(derived >= 0 && derived < PL_NUM_CLASS,
@@ -85,6 +90,7 @@ static pl_unittest_summary test_type(void)
 
     pl_unittest_expect_error_is(summary, PL_ERROR_UNDEFINED_CLASS, type(PL_NUM_CLASS));
     pl_unittest_expect_error_is(summary, PL_ERROR_UNDEFINED_CLASS, type(-1));
+
     pl_unittest_expect_true(summary, type(PL_CLASS_CHAR) == PL_CLASS_CHAR);
 
     return summary;
@@ -100,6 +106,7 @@ static void test(void)
     pl_unittest_print_summary(test_inherit());
     pl_unittest_print_summary(test_type());
 }
+
 
 pl_class_ns pl_class_get_ns(void)
 {
